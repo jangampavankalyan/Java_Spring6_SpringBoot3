061e805dd1b1c6ae7e4492542830ae7f65dd6856
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-03-11 09:55:46 +0530

    Start of Spring-Data-Rest-Demo

---
+++ Spring-Data-Rest-Demo/src/main/java/com/example/Spring_Data_Rest_Demo/SpringDataRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Data_Rest_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestDemoApplication.class, args);
+	}
+
+}
---
+++ Spring-Data-Rest-Demo/src/test/java/com/example/Spring_Data_Rest_Demo/SpringDataRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Data_Rest_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
