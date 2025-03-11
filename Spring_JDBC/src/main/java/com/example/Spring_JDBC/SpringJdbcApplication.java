1284da6aab18cc9a090468459ded3d63ec1c8986
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-03-11 14:55:52 +0530

    Creating a Spring JDBC Project

--- Spring_JDBC/src/main/java/com/example/Spring_JDBC/SpringJdbcApplication.java
+++ Spring_JDBC/src/main/java/com/example/Spring_JDBC/SpringJdbcApplication.java
@@ -1,13 +1,20 @@
 package com.example.Spring_JDBC;
 
+import com.example.Spring_JDBC.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+
+		Student s = context.getBean(Student.class);
+		s.setRollNo(101);
+		s.setName("Pavan Kalyan");
+		s.setMarks(90);
 	}
 
 }
