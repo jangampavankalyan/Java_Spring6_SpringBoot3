5014437acfb239acdd7d41925d725a4e72a934c5
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-03-11 15:42:01 +0530

    Spring JDBC Postgres

--- Spring_JDBC/src/main/java/com/example/Spring_JDBC/repo/StudentRepo.java
+++ Spring_JDBC/src/main/java/com/example/Spring_JDBC/repo/StudentRepo.java
@@ -6,9 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
