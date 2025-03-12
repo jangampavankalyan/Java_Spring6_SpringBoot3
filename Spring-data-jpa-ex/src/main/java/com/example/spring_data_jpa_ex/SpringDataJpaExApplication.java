29e9c658e0bd96ca9249eac450f58a178d595ec2
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-03-12 10:10:39 +0530

    Findall

--- Spring-data-jpa-ex/src/main/java/com/example/spring_data_jpa_ex/SpringDataJpaExApplication.java
+++ Spring-data-jpa-ex/src/main/java/com/example/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -17,21 +17,24 @@ public class SpringDataJpaExApplication {
 		Student s1 = context.getBean(Student.class);
 		Student s2 = context.getBean(Student.class);
 		Student s3 = context.getBean(Student.class);
+//
+//		s1.setRollNo(101);
+//		s1.setName("Navin");
+//		s1.setMarks(75);
+//
+//		s2.setRollNo(102);
+//		s2.setName("Kiran");
+//		s2.setMarks(80);
+//
+//		s3.setRollNo(103);
+//		s3.setName("Harsh");
+//		s3.setMarks(70);
+//
+//		repo.save(s1);
+//		repo.save(s2);
+//		repo.save(s3);
 
-		s1.setRollNo(101);
-		s1.setName("Navin");
-		s1.setMarks(75);
-
-		s2.setRollNo(101);
-		s2.setName("Navin");
-		s2.setMarks(75);
-
-		s3.setRollNo(101);
-		s3.setName("Navin");
-		s3.setMarks(75);
-
-		repo.save(s1);
+		System.out.println(repo.findAll());
 
 	}
-
 }
