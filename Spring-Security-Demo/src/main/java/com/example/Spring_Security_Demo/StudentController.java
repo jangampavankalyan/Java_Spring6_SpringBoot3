26c2ca59fb9875eaa419365154d963ac6b6cafdd
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-03-18 15:22:52 +0530

    Progress on Spring Security - 2

--- Spring-Security-Demo/src/main/java/com/example/Spring_Security_Demo/StudentController.java
+++ Spring-Security-Demo/src/main/java/com/example/Spring_Security_Demo/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.Spring_Security_Demo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,11 @@ public class StudentController {
     List<Student> students = new ArrayList<>(List.of(new Student(1,"Navin","Java"),
             new Student(2,"Kiran","Blockchain")
     ));
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
 
     @GetMapping("/students")
     public List<Student> getStudents() {
