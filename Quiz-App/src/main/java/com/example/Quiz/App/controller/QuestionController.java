6e66d4469cd4051554c9064c551349524abc8e75
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-04-29 23:57:17 +0530

    Progress on Quiz App - 2

--- Quiz-App/src/main/java/com/example/Quiz/App/controller/QuestionController.java
+++ Quiz-App/src/main/java/com/example/Quiz/App/controller/QuestionController.java
@@ -3,10 +3,11 @@ package com.example.Quiz.App.controller;
 import com.example.Quiz.App.model.Question;
 import com.example.Quiz.App.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,35 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("/allQuestions")
-    public List<Question> getAllQuestions(){
-        return questionService.getAllQuestions();
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try {
+            return new ResponseEntity<>(questionService.getAllQuestions(), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+        try {
+            return new ResponseEntity<>(questionService.getQuestionsByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return new ResponseEntity<>(questionService.addQuestion(question),HttpStatus.CREATED);
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<String> updateQuestion(@RequestBody Question question){
+        return new ResponseEntity<>(questionService.updateQuestion(question),HttpStatus.CREATED);
+    }
+
+//    @DeleteMapping("delete")
+//    public ResponseEntity<String> deleteQuestion()
 }
