6eec0bc091181c0fc72492167f2dd4b6432aa772
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-05-03 20:30:06 +0530

    Complete of Quiz MicroService

--- Quiz-MicroService/quiz-service/src/main/java/com/example/quiz_service/service/QuizService.java
+++ Quiz-MicroService/quiz-service/src/main/java/com/example/quiz_service/service/QuizService.java
@@ -1,6 +1,7 @@
 package com.example.quiz_service.service;
 
 import com.example.quiz_service.dao.QuizDao;
+import com.example.quiz_service.feign.QuizInterface;
 import com.example.quiz_service.model.QuestionWrapper;
 import com.example.quiz_service.model.Quiz;
 import com.example.quiz_service.model.Response;
@@ -19,37 +20,30 @@ public class QuizService {
     @Autowired
     QuizDao quizDao;
 
+    @Autowired
+    QuizInterface quizInterface;
+
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
-//        Quiz quiz = new Quiz();
-//        quiz.setTitle(title);
-//        quiz.setQuestions();
-//        quizDao.save(quiz);
+        List<Integer> questions = quizInterface.getQuestionsForQuiz(category,numQ).getBody();
+
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestionIds(questions);
+
+        quizDao.save(quiz);
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-        Optional<Quiz> quiz = quizDao.findById(id);
-//        List<Question> questionsFromDB = quiz.get().getQuestions();
-        List<QuestionWrapper> questionsForUser = new ArrayList<>();
-//        for(Question q : questionsFromDB){
-//            QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
-//            questionsForUser.add(qw);
-//        }
-        return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
+        Quiz quiz = quizDao.findById(id).get();
+        List<Integer> questionIds = quiz.getQuestionIds();
+        ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
+        return questions;
     }
 
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
-        Optional<Quiz> quiz = quizDao.findById(id);
-//        List<Question> questions = quiz.get().getQuestions();
-//
-          int right = 0;
-//        int i=0;
-//        for (Response response : responses){
-//            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
-//                right++;
-//            i++;
-//        }
-        return new ResponseEntity<>(right,HttpStatus.OK);
+        ResponseEntity<Integer> score = quizInterface.getScore(responses);
+        return score;
 
     }
 }
