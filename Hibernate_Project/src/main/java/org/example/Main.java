f63d223c33e984c3bae7982708d04d896d3a74c5
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-05-13 12:00:54 +0530

    Progress on Hibernate - 2

--- Hibernate_Project/src/main/java/org/example/Main.java
+++ Hibernate_Project/src/main/java/org/example/Main.java
@@ -1,9 +1,31 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
 
         Student s1 = new Student();
+        s1.setRollno(1);
+        s1.setsName("Pavan kalyan");
+        s1.setsAge(21);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(org.example.Student.class);
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(s1);
+
+        transaction.commit();
+
         System.out.println(s1);
     }
 }
