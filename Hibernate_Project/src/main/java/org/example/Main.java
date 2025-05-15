0ed02275baf434d6a7548d9edb65cd50fdb79b4e
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-05-15 15:09:06 +0530

    Progress on Hibernate - 6

--- Hibernate_Project/src/main/java/org/example/Main.java
+++ Hibernate_Project/src/main/java/org/example/Main.java
@@ -8,34 +8,26 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
 
-        StudentModel s1 = new StudentModel();
-        s1.setRollno(1);
-        s1.setsName("Pavan kalyan");
-        s1.setsAge(22);
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Pavan Kalyan");
+        a1.setTech("Java");
 
-        StudentModel s2 = null;
-
-        SessionFactory sf = new Configuration()
-                .addAnnotatedClass(StudentModel.class)
+       SessionFactory sf = new Configuration()
+                .addAnnotatedClass(Alien.class)
                 .configure()
                 .buildSessionFactory();
 
         Session session = sf.openSession();
 
-        s2 = session.get(StudentModel.class,1);
         Transaction transaction = session.beginTransaction();
 
-        //session.persist(s1);
-
-//        session.merge(s1);
-
-//        session.remove(s2);
+        session.persist(a1);
 
         transaction.commit();
 
         session.close();
         sf.close();
 
-        System.out.println(s1);
     }
 }
