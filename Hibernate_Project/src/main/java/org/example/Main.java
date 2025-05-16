c93a642298fd17e9aa220cef17f020a9ba4ed534
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-05-16 16:21:11 +0530

    Progress on Hibernate - 12

--- Hibernate_Project/src/main/java/org/example/Main.java
+++ Hibernate_Project/src/main/java/org/example/Main.java
@@ -2,13 +2,7 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,28 +15,14 @@ public class Main {
 
         Session session = sf.openSession();
 
-//        Query query = session.createQuery("from Laptop where ram=32");
+//        Laptop laptop = session.get(Laptop.class,1);
+//        System.out.println(laptop);
+//
+//        Laptop laptop = session.load(Laptop.class,1);
+//        System.out.println(laptop);
 
-        String brand = "Asus";
-//        Query query = session.createQuery("from Laptop where brand like ?1");
-//        query.setParameter(1,brand);
-//        List<Laptop> laptops = query.getResultList();
-
-
-        Query query = session.createQuery("select brand,model from Laptop where brand like ?1");
-        query.setParameter(1,brand);
-        List<Object[]> laptops = query.getResultList();
-
-        for(Object[] data : laptops){
-            System.out.println((String) data[0] + " " + (String) data[1]);
-        }
-
-
-//        Laptop l1 = session.get(Laptop.class,1);
-
-//        System.out.println(l1);
-
-        System.out.println(laptops);
+        Laptop laptop = session.byId(Laptop.class).getReference(1);
+//        System.out.println(laptop);
 
         session.close();
         sf.close();
